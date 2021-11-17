6c664f2dd74c04ad9d9475ab12b04fdece38f0d1
Mbandi Anyere <mbandi@Mbandis-MacBook-Pro.local>
2021-11-17 18:32:56 -0500

    tf file/remote provisioners

--- ec2.tf
+++ ec2.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "my-ec2-vm" {
     destination = "/tmp/apache-install.sh"
   }
 
+ /*
   provisioner "remote-exec" {
     inline = [
       "sleep 120",  # Will sleep for 120 seconds to ensure Apache webserver is provisioned using user_data
@@ -36,29 +37,6 @@ resource "aws_instance" "my-ec2-vm" {
       "sudo systemctl enable httpd"
       ]
   }
-  /*
-  # Copies the app1 folder to /tmp - FOLDER COPY
-  provisioner "file" {
-    source      = "apps/app1"
-    destination = "/tmp"
-  }
-
-  # Copies all files and folders in apps/app2 to /tmp - CONTENTS of FOLDER WILL BE COPIED
-  provisioner "file" {
-    source      = "apps/app2/" # when "/" at the end is added - CONTENTS of FOLDER WILL BE COPIED
-    destination = "/tmp"
-  }
-*/
-  /*
-# 2nd TEST with failures "Continue" or "fail"
-# Enable this during Section 09-01 Step-05
- # Copies the file-copy.html file to /var/www/html/file-copy.html where ec2-user don't have permission to copy. The user needs "sudo" permissions
- # This provisioner will fail but we don't want to taint the resource, we want to continue on_failure
-  provisioner "file" {
-    source      = "apps/file-copy.html"
-    destination = "/var/www/html/file-copy.html"  # With this terraform will first fail "Permission Denied", If you run terraform apply again it will "tain" the resource and destroys and recreate.
-    #on_failure  = continue  # Uncomment this during Test-2 if you don't want terraform to destroy the resource
-   }
 */
 }
 
